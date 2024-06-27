7aaaa5c7901b17e66c6a062b261888f1334442a0
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-27 17:59:38 -0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
     extend: {
       colors: {
         'col-DB': 'hsl(240, 38%, 20%)',
-        'col-GB': 'hsl(240, 18%, 77%)'
+        'col-GB': 'hsl(240, 18%, 77%)',
+        'col-body': 'hsl(0, 0, 100)'
       },
       fontFamily: {
         Inter: 'Inter'
+      },
+      backgroundImage: {
+        'Img-pattern-bg': "url('/src/assets/images/pattern-bg.svg')",
+        'Img-pattern-curve': "url('/src/assets/images/pattern-curve.svg')",
+        'Img-pattern-quotes': "url('/src/assets/images/pattern-quotes.svg')",
+        'Img-tanya': "url('/src/assets/images/image-tanya.jpg')"
+      },
+      screens: {
+        xs: '400px'
       }
     },
   },
